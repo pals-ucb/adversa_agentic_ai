4a07cd9e57a0caed0e5269fb89f7c04fa90a2a08
pals chinnakannan <palsc@berkeley.edu>
2025-06-07 10:14:14 -0500

    Adding SAM support setting up API Server in AWS Lambda
    Signed-off-by: pals chinnakannan <palsc@berkeley.edu>

--- src/utils/config_logger.py
+++ src/utils/config_logger.py
@@ -10,23 +10,32 @@ def set_current_agent(name: str):
     global _agent_name
     _agent_name = name
 
-
 def setup_logger(app_name: str) -> logging.Logger:
-    workspace_root = find_workspace_root()
-    log_dir = os.path.join(workspace_root, "logs")
-    os.makedirs(log_dir, exist_ok=True)
-
-    log_file_path = os.path.join(log_dir, f"{app_name}.log")
-
-    log_format = "[%(asctime)s] %(levelname)s: %(message)s"
-    logging.basicConfig(
-        level=logging.INFO,
-        format=log_format,
-        handlers=[
-            logging.StreamHandler(),
-            logging.FileHandler(log_file_path)
-        ]
-    )
+    if os.environ.get("AWS_LAMBDA_FUNCTION_NAME"):
+        # Running in Lambda
+        log_format = "[%(levelname)s: %(message)s"
+        logging.basicConfig(
+            level=logging.INFO,
+            format=log_format,
+            handlers=[
+                logging.StreamHandler()
+            ]
+        )
+    else:
+        # Running locally
+        log_format = "[%(asctime)s] %(levelname)s: %(message)s"
+        workspace_root = find_workspace_root()
+        log_dir = os.path.join(workspace_root, "logs")
+        os.makedirs(log_dir, exist_ok=True)
+        log_file_path = os.path.join(log_dir, f"{app_name}.log")
+        logging.basicConfig(
+            level=logging.INFO,
+            format=log_format,
+            handlers=[
+                logging.StreamHandler(),
+                logging.FileHandler(log_file_path)
+            ]
+        )
     return logging.getLogger(app_name)
 
 def get_agent_logger():
